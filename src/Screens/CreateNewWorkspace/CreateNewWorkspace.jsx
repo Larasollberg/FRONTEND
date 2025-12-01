8ffeb01a4e8721879db0ae01328dbd41e5f2abfc
larasollberg <larasollberg@gmail.com>
2025-11-30 23:07:44 -0300

    css

--- src/Screens/CreateNewWorkspace/CreateNewWorkspace.jsx
+++ src/Screens/CreateNewWorkspace/CreateNewWorkspace.jsx
@@ -13,22 +13,22 @@ const CreateNewWorkspace = () => {
 
     return (
         <div className="createNewWorkspace-container">
-        <div className="createNewWorkspace-logo">
-            <img src={slackLogo} alt="Slack logo" />
-        </div>
-        <div className="createNewWorkspace-title-subtitle">
-            <h2 className="createNewWorkspace-title">¡Hola de nuevo!</h2>
-            <p className="createNewWorkspace-subtitle">
-            Crear un nuevo espacio de trabajo
-            </p>
-        </div>
-        <div className="createNewWorkspace--button">
-            <button className="createNewWorkspace-button" onClick={handleContinue}>
-            <span className="button-icon">+</span>
-            <span className="button-text">Crea un nuevo espacio de trabajo</span>
-            <span className="button-arrow">→</span>
-            </button>
-        </div>
+            <div className="createNewWorkspace-logo">
+                <img src={slackLogo} alt="Slack logo" />
+            </div>
+            <div className="createNewWorkspace-title-subtitle">
+                <h2 className="createNewWorkspace-title">¡Hola de nuevo!</h2>
+                <p className="createNewWorkspace-subtitle">
+                Crear un nuevo espacio de trabajo
+                </p>
+            </div>
+            <div className="createNewWorkspace--button">
+                <button className="createNewWorkspace-button" onClick={handleContinue}>
+                <span className="button-icon">+</span>
+                <span className="button-text">Crea un nuevo espacio de trabajo</span>
+                <span className="button-arrow">→</span>
+                </button>
+            </div>
         </div>
     );
     };
